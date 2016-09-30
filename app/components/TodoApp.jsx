6e042f7fd3f5a854a04df370d873c41d3686639b
cyilcode <cmylmzbm@outlook.com>
2016-09-30 21:23:22 +0300

    Added static todo components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,33 @@
 var React = require('react');
-
+var TodoList = require('TodoList');
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return { // Dummy load to work with.
+      todos: [
+        {
+          id: 1,
+          text: 'remember to like trains'
+        },
+        {
+          id: 2,
+          text: 'do not touch his tralala'
+        },
+        {
+          id: 3,
+          text: 'and dont forget the joker'
+        },
+        {
+          id: 4,
+          text: 'remember to remember when you cant really remember'
+        }
+      ]
+    };
+  },
   render: function() {
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos} />
       </div>
     );
   }
---
+++ app/components/TodoList.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var Todo = require('Todo');
+var TodoList = React.createClass({
+  render: function() {
+    var {todos} = this.props;
+    var renderTodos = () => {
+      return todos.map((item) => {
+        return (
+          <Todo key={item.id} {...item}/> // spread operator ftw
+        );
+      });
+    };
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    );
+  }
+});
+
+module.exports = TodoList;
