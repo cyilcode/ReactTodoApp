5df4b9bcc4b53018076b84f7ea27e358ee7bf487
cyilcode <cmylmzbm@outlook.com>
2016-09-30 21:49:57 +0300

    Add tests for todo components

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jQuery');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+describe('TodoList', () => {
+    it('should exist', () => {
+        expect(TodoList).toExist();
+    });
+
+    it('should render one Todo component for each todo item', () => {
+        var todoitems = [
+          {
+            id: 1,
+            text: 'test1'
+          },
+          {
+            id: 2,
+            text: 'test2'
+          }
+        ];
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todoitems} />);
+        var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+        expect(todoitems.length).toBe(todosComponents.length);
+    });
+});
