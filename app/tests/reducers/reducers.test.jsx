b6feb2a97ffef5be3d4e46daad10cc5ea676f1db
cyilcode <cmylmzbm@outlook.com>
2016-10-05 23:47:26 +0300

    Added Todo reducers and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -23,4 +23,30 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      const text_to_set = 'h3ll0000';
+      var action = { type: 'ADD_TODO', text: text_to_set };
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length > 0).toEqual(true);
+      expect(res[0].text).toEqual(text_to_set);
+    });
+
+    it('should complete a todo', () => {
+      const object_id = 1;
+      const object_array = [{
+        id: 1,
+        text: 'wow i am done',
+        completed: false,
+        createdAt: 12345,
+        completedAt: undefined
+      }];
+      var action = { type: 'TOGGLE_TODO', id: object_id };
+      var res = reducers.todosReducer(object_array, df(action)); // fl!p fl0p
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toBeA('number');
+    });
+
+  });
 });
